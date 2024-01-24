2495a5c5ff1303d6eff5647d7af997f25ba78a83
Adrian Breiding <ad123br@gmail.com>
2024-01-24 15:57:05 +0100

    Setup DCSC

--- blatt8/distributed/DCSCMatrix.cpp
+++ blatt8/distributed/DCSCMatrix.cpp
@@ -1,5 +1,28 @@
 #include "DCSCMatrix.hpp"
+#include <cmath>
+
+class DCSCMatrix
+{
+private:
+    /* data */
+public:
+    DCSCMatrix(/* args */);
+    ~DCSCMatrix();
+};
+
+DCSCMatrix::DCSCMatrix(/* args */)
+{
+}
 
 DCSCMatrix::DCSCMatrix(size_t rows, size_t cols, std::vector<Triplet> triplets){
-    
+    // Wir gehen an dieser Stelle zunächst davon aus, dass die Anzahl der Prozesse p bekannst ist.
+    // Dabei kann davon ausgegangen werden, dass p eine Quadratzahl ist.
+    int p = 25;
+    int p_sqrt = std::sqrt(p);
+
+
 } 
+
+DCSCMatrix::~DCSCMatrix()
+{
+}
