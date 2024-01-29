5beff42fb46815bcb80dbcd006804cd7fd3a8cbb
Adrian Breiding <ad123br@gmail.com>
2024-01-29 20:23:41 +0100

    Commentary

--- blatt8/distributed/DCSCMatrix.cpp
+++ blatt8/distributed/DCSCMatrix.cpp
@@ -22,6 +22,7 @@ DCSCMatrix::DCSCMatrix(size_t rows, size_t cols, std::vector<Triplet> triplets){
     double sqrt_p = std::sqrt(p);
     std::vector<Triplet> subvector_triplets;
     for (Triplet triplet : triplets){
+        // A subvector with the relevant entries for each process is created
         if (rank % (int (sqrt_p)) == std::floor(triplet.col / s) && std::floor(rank / sqrt_p) == std::floor(triplet.row / s)){
             subvector_triplets.push_back({triplet.row % s, triplet.col % s, triplet.value});
         }
@@ -30,26 +31,23 @@ DCSCMatrix::DCSCMatrix(size_t rows, size_t cols, std::vector<Triplet> triplets){
 } 
 
 DVector DCSCMatrix::operator*(const DVector &rhs) const {
-	/*// Dimension Verification, returns unchanged vector on error
-    if (rhs.size() != cols){
-        throw std::invalid_argument("Dimension mismatch!");
-        return rhs;
-    }*/
-    std::cout << "Test";
     int comm_size;
     int comm_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
+    // Calculation of local products
     DenseVector local_result = data * rhs.data;
+    // Choose a manager process for each row
     int local_manager = comm_rank - (comm_rank % int(std::sqrt(comm_size)));
     std::vector<double> result(rhs.data.size() * int(std::sqrt(comm_size)));
-    std::cout << comm_rank << ": " << rhs.data << "\n";
     if (local_manager != comm_rank) {
+        // the local worker processes send their results to the local manager
         for (int i = 0; i < local_result.size(); i++){
             int current = local_result(i);
             MPI_Send(&current, 1, MPI_INT, local_manager, 0, MPI_COMM_WORLD);
         }
     } else {
+        // the local manager receives the results from the local workers
         for (int i = 0; i < local_result.size(); i++){
             for (int j = 1; j < int(std::sqrt(comm_size)); j++) {
                 int current_recv = 0;
@@ -57,13 +55,14 @@ DVector DCSCMatrix::operator*(const DVector &rhs) const {
                 local_result(i) += current_recv;
             }
         }
-
+        // and send the aggregated results to the root process
         if (comm_rank != 0) {
             for (int i = 0; i < local_result.size(); i++){
                 int current = local_result(i);
                 MPI_Send(&current, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
             }
         } else {
+            // the root process stores all of the local results in a vector of length n
             for (int i = 0; i < local_result.size(); i++) {
                 result[i] = local_result(i);
             }
@@ -76,23 +75,24 @@ DVector DCSCMatrix::operator*(const DVector &rhs) const {
                     result[local_result.size() * j + i] = current_recv;
                 }
             }
+            // Since every Process needs to return a DVector, the result is communicated with the other processes
             for (int j = 0; j < result.size(); j++){
                 int current = result.at(j);
                 for (int i = 0; i < comm_size; i++) {
                     MPI_Send(&current, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
                 }
-                //MPI_Bcast(&current, 1, MPI_INT, 0, MPI_COMM_WORLD);
             } 
         }
     }
     if(comm_rank > 0){
+        // The non root processes receive the results of the computation
         for (int j = 0; j < result.size(); j++){
             int current_recv = 0;
             MPI_Recv(&current_recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             result[j] = current_recv;
         } 
     }
-    std::cout << comm_rank;
+    // return of final result 
     return DVector(result);
 }
 DCSCMatrix::~DCSCMatrix()
--- blatt8/distributed/DVector.cpp
+++ blatt8/distributed/DVector.cpp
@@ -23,18 +23,19 @@ DVector::DVector(std::vector<double> init) {
 }
 
 std::ostream &operator<<(std::ostream &os, const DVector &vec) {
-  // TODO: implement print for DVector. Printing should include global indices
-  // for each element in the vector
   if (vec.comm_rank == 0){
+    // Since it's easier to coordinate, the printing is being done in the root process
     os << "(";
     os << vec.data(0);
     for (int i = 1; i < vec.data.size(); i++){
+      // printing of local data
       os << ", ";
       os << vec.data(i);
     } 
     int sqrt_size = int(std::sqrt(vec.comm_size));
     for (int i = 1; i < sqrt_size; i++){
       for (int j = 0; j < vec.data.size(); j++){
+        // receives and prints the entries in ascending order from the subprocesses
         int received = 0;
         MPI_Recv(&received, 1, MPI_INT, i , 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         os << ", ";
@@ -45,6 +46,7 @@ std::ostream &operator<<(std::ostream &os, const DVector &vec) {
   } else if (vec.comm_rank / int(std::sqrt(vec.comm_size)) == 0)
   {
     for (int i = 0; i < vec.data.size(); i++){
+      // send the local data to the root process
       int current = vec.data(i);
       MPI_Send(&current, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     } 
